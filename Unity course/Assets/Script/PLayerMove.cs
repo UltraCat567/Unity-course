872e6f0924a249fb7e7726093aa1bb1e8d76cbdd
UltraCat567 <sonihkabobop@gmail.com>
2025-01-18 01:24:20 +0300

    Animations

--- Unity course/Assets/Script/PLayerMove.cs
+++ Unity course/Assets/Script/PLayerMove.cs
@@ -7,10 +7,8 @@ public class PLayerMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _rotate;
-    void Start()
-    {
+    [SerializeField] private Animator _animator;
 
-    }
 
     // Update is called once per frame
     void Update()
@@ -22,18 +20,25 @@ public class PLayerMove : MonoBehaviour
             newPosition.x = Mathf.Clamp(newPosition.x, -2.7f, 2.7f);
             transform.position = newPosition;
 
+            _animator.SetBool("Run", true);
+
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0, - _rotate * Time.deltaTime, 0);
+            transform.Rotate(0, -_rotate * Time.deltaTime, 0);
 
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, _rotate * Time.deltaTime, 0);
-       
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            _animator.SetBool("Run", false);
         }
     }
 }
